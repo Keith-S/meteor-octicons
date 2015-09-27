d643401a58dd5f3a7f599837783ed4ad6db7cd05
Keith <keith.sparks2@gmail.com>
2015-09-27 09:51:29 -0400

    fixed updated version #

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Octicons library for Meteor.js',
-  version: '0.4.2',
+  version: '0.5.0',
   name: "keiths:octicons",
   git: 'https://github.com/Keith-S/meteor-octicons'
 });
