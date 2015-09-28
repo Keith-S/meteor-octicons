f692745cc520926f901d959a620fbf5b5649bf7b
Keith <keith.sparks2@gmail.com>
2015-09-27 22:00:15 -0400

    Fixed api calls & bumped version number

--- package.js
+++ package.js
@@ -1,16 +1,18 @@
 Package.describe({
   summary: 'Octicons library for Meteor.js',
-  version: '0.5.0',
+  version: '0.5.1',
   name: "keiths:octicons",
   git: 'https://github.com/Keith-S/meteor-octicons'
 });
 
-Package.on_use(function (api) {
-    api.add_files([
-        "lib/fonts/octicons.eot",
-        "lib/fonts/octicons.svg",
-        "lib/fonts/octicons.ttf",
-        "lib/fonts/octicons.woff",
-        "lib/css/octicons.css"
+Package.onUse(function (api) {
+    api.addAssets([
+      "lib/fonts/octicons.eot",
+      "lib/fonts/octicons.svg",
+      "lib/fonts/octicons.ttf",
+      "lib/fonts/octicons.woff"
     ], "client");
+    api.addFiles([
+      "lib/css/octicons.css"
+    ], 'client');
 });
